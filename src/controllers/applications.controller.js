7eba7d62414e375502dec2cb8fc20875add02be8
Rakib Mondal <rakib8feb@gmail.com>
2026-02-05 21:38:25 +0530

    fix: return structured data from  get job application

--- src/controllers/applications.controller.js
+++ src/controllers/applications.controller.js
@@ -1,23 +1,26 @@
 const Application = require("../models/Application");
 
-exports.createApplication = async (req,res)=>{
-    try{
+exports.createApplication = async (req, res) => {
+    try {
         const application = await Application.create(req.body);
-        res.status(201).json({message:"Application created successfully!"});
+        res.status(201).json({ message: "Application created successfully!" });
     }
-    catch (err){
+    catch (err) {
         console.log(err);
-        res.status(500).json({message: "Failed to create a new application"});
+        res.status(500).json({ message: "Failed to create a new application" });
     }
 }
 
-exports.getApplications = async (req,res)=>{
-    try{
+exports.getApplications = async (req, res) => {
+    try {
         const applications = await Application.find();
         console.log(applications);
-        res.status(201).json({message: applications})
+        res.status(201).json({ 
+            success:true,
+            count:applications.length,
+            data: applications })
     }
-    catch(err){
-        res.status(500).json({message:"failed to get applications"});
+    catch (err) {
+        res.status(500).json({ message: "failed to get applications" });
     }
 }
